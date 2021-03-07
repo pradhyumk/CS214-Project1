1b6937678c6a52395fa309dc1fb754553bfdd2f3
Pradhyum Krishnan <pradhyum.krishnan@rutgers.edu>
2021-03-07 17:57:54 -0500

    Added method to convert file/stdin input to a char array.
    Co-authored-by: Gaurav Patel <GauravP2001@users.noreply.github.com>

--- ww.c
+++ ww.c
@@ -6,34 +6,52 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define isDirectory 0;
 
-// int isdir(char *name) {
-// 	struct stat data;
+typedef struct {
+    size_t length;
+	size_t used;
+    char *data;
+} strbuff_t;
+
+void sb_destroy(char *L) {
+    free(L);
+}
+
+int sb_append(strbuff_t *sb, char item)
+{
+    if (sb->used == sb->length) {
+	   size_t size = sb->length * 2;
+	   char *p = realloc(sb->data, sizeof(char) * size);
+	   if (!p) return 1;
+
+	   sb->data = p;
+	   sb->length = size;
+    }
+
+    sb->data[sb->used] = sb->data[sb->used-1];
+    sb->data[sb->used-1] = item;
+
+    ++sb->used;
+    
+    return 0;
+}
+
+/* String: hello    world
+ * width: 3
+ * create method that gets the length of the word before printing it
+ */
+
+int lastCharacter(int start, int width, char* buffer){
+	int counter = 0;
+	int ws = 0;
+	int word = 0;
+	int laststartIndex = start;
 	
-// 	int err = stat(name, &data);
+	while (counter < width) {
+		
+	}
 	
-// 	// should confirm err == 0
-// 	if (err) {
-// 		perror(name);  // print error message
-// 		return 0;
-// 	}
 	
-// 	if (S_ISDIR(data.st_mode)) {
-// 		// S_ISDIR macro is true if the st_mode says the file is a directory
-// 		// S_ISREG macro is true if the st_mode says the file is a regular file
-//         #undef isDirectory
-//         #define isDirectory 1
-
-// 		return 1;
-// 	} 
-
-// 	return 0;
-// }
-
-
-int lastCharacter(int start, int width, int fd, char* buffer){
-
     return 0;
 }
 
@@ -41,26 +59,53 @@ void ww(int width, int fd_in, char* buffer) {
     lastCharacter(0, width, fd_in, buffer);
 }
 
-int main(int argc, char **argv) {
-    printf("Filename: %s\n", argv[2]);
-	
-	int fd = open(argv[2], O_RDONLY);
-	printf("fd: %d\n", fd);
+char* charArray(int fd) {
+	char ch;
+	strbuff_t sb;
+	sb.data = malloc(sizeof(char));
+	sb.data[0] = '\0';
+	sb.length = 1;
+	sb.used   = 1;
 
-	unsigned int width = atoi(argv[1]);
-	printf("Width: %d\n", width);
+	while(read(fd, &ch, 1) != 0) {
+		sb_append(&sb, ch);
+	}
 
-	char data;
-
-	ww(width, fd, &data);
-
-    char c;
-
-    while (read(fd, &c, 1) == 1){
-        putchar(c);
+	if (fd > 2)
+	{
+		close(fd);
 	}
 	
+	return sb.data;
+}
+
+int main(int argc, char **argv) {
+
+	int fd = 0;
+	char* data;
+	
+	if (argc == 3) {
+		fd = open(argv[2], O_RDONLY); 
+	} else if (argc == 2) {
+		char c[] = "Please enter your text: \n";
+		write(1, c, sizeof(c)-1);
+	} else {
+		return EXIT_FAILURE;
+	}
+
+	 
+	data = charArray(fd);
+	printf("Array: %s\n", data);
+
+	unsigned int width = atoi(argv[1]);
+
+	printf("Width: %d\n", width);
+
+
 	printf("\n");
 
-	close(fd);
+	sb_destroy(data);
+
+
+	return EXIT_SUCCESS;
 }
