b428d5d6f1a0c0ea77ab865c0e771b6cffe96773
Pradhyum Krishnan <pradhyum.krishnan@rutgers.edu>
2021-03-09 19:21:43 -0500

    Whitespace handling
    Co-authored-by: Gaurav Patel <GauravP2001@users.noreply.github.com>

--- ww.c
+++ ww.c
@@ -11,6 +11,8 @@ typedef struct {
     size_t length;
 	size_t used;
     char *data;
+
+	// h e l l o \0 _  _  _  _  
 } strbuff_t;
 
 void sb_destroy(char *L) {
@@ -36,53 +38,128 @@ int sb_append(strbuff_t *sb, char item)
     return 0;
 }
 
-/* String: hello    world
- * width: 3
- * create method that gets the length of the word before printing it
+
+int wordLength(int index, strbuff_t *sb){
+	
+	char *buffer = sb->data;
+
+	if (isspace(buffer[index]))	{
+		return -1;
+	}
+
+	int counter = index;
+	int length = 0;
+	while (counter <  sb->used){
+		
+		if (isspace(buffer[counter])) {
+			break;
+		}
+		
+		length++;
+		counter++;
+	}
+	
+    return length;
+}
+
+// \n  \n\n     hello world --
+//hello\n\t\t\nworld --
+//hello\n\n\t\t\n\nworld --
+//hello \n\t world --
+//hello --
+//hello      world
+
+/* 
+ * hello
+ * 
+ * world
+ * hello  \n world (w=8)
  */
 
-int lastCharacter(int start, int width, char* buffer){
-	int counter = 0;
-	int ws = 0;
-	int word = 0;
-	int laststartIndex = start;
-	
-	while (counter < width) {
-		
+
+void ww(int width, int fd_in, strbuff_t *sb) {
+	int i = 0;	// array index
+	int characterUsed = 0; // Characters  in the current line
+	int whiteSpace = 0;
+	int wordLen;
+
+	wordLen = wordLength(4, sb);
+	printf("Length of the word is: %d\n", wordLen);
+
+	while (i < sb->used) { // going through every character one by one
+		if (isspace(sb->data[i]) && characterUsed != 0) {	// checking for 2 or more lines at the start AND if the \n is not at the beggining for a sentence
+
+			int printed = 0; // to make sure we only print one para break
+
+			if (sb->data[i] == '\n') {
+				i++; // start from next character
+				
+				while (isspace(sb->data[i])) {
+					if (sb->data[i] == '\n' && printed == 0) { 	// saw two \n and printing a paragraph break
+						write(fd, "\n\n", 2);
+						characterUsed = 0;
+						printed = 1;
+						i++;
+					} else if (isspace(sb->data[i])) {
+						i++;
+					} 
+				}
+
+			} else {
+				if (characterUsed == 0) { // if there is a whitepace at the begging of a line
+					i++;
+				} 
+			}
+
+			if (printed == 0) {
+				int nextword = wordLength(i, &sb);	// gettting the length of the word to see if it fits; it it fits, we print the space
+
+				if (nextword+1 <= width - characterUsed){
+					write(fd, " ", 1);
+					characterUsed++;
+				} else {
+					write(fd, "\n", 1);
+					characterUsed = 0;
+				}
+			}
+		} else if (!isspace(sb->data[i])){ // character
+
+
+			// print white space
+
+			while () { //starts at the current index and prints everything until it hits a whitespace
+
+			}
+		} else {
+			i++;
+		}
 	}
-	
-	
-    return 0;
 }
 
-void ww(int width, int fd_in, char* buffer) {
-    lastCharacter(0, width, fd_in, buffer);
-}
 
-char* charArray(int fd) {
+strbuff_t* charArray(int fd, strbuff_t *sb) {
 	char ch;
-	strbuff_t sb;
-	sb.data = malloc(sizeof(char));
-	sb.data[0] = '\0';
-	sb.length = 1;
-	sb.used   = 1;
+	sb->data = malloc(sizeof(char));
+	sb->data[0] = '\0';
+	sb->length = 1;
+	sb->used   = 1;
 
 	while(read(fd, &ch, 1) != 0) {
-		sb_append(&sb, ch);
+		sb_append(sb, ch);
 	}
 
-	if (fd > 2)
-	{
+	if (fd > 2) {
 		close(fd);
 	}
 	
-	return sb.data;
+	return sb;
 }
+
 
 int main(int argc, char **argv) {
 
 	int fd = 0;
-	char* data;
+	strbuff_t sb;
 	
 	if (argc == 3) {
 		fd = open(argv[2], O_RDONLY); 
@@ -93,19 +170,30 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	 
-	data = charArray(fd);
-	printf("Array: %s\n", data);
-
 	unsigned int width = atoi(argv[1]);
-
+	 
+	charArray(fd, &sb);
+	
+	printf("---------- Document Array ----------\n");
+	printf("%s\n", sb.data);
+	printf("------------------------------------\n");
 	printf("Width: %d\n", width);
+	printf("Document length: %lu\n", sb.used);
+
+	ww(width, fd, &sb);
 
 
-	printf("\n");
-
-	sb_destroy(data);
+	sb_destroy(sb.data);
 
 
 	return EXIT_SUCCESS;
 }
+
+
+			// if ((i+1 < sb->used) && (sb->data[i+1] == '\n')) {
+			// 	while (!ispace(sb->data[i+1])) { // print a new line until you see that next character is not a character
+			// 		write(fd_in, )
+
+			// 		i += 2;
+			// 	}
+			// }
